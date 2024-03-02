a8b0fff4b08fc8c7cae7a6ef47a29671be94d340
ValeriaIbragimova <valeria92-92+1@mail.ru>
2024-03-02 19:48:15 +0300

    2.2.1 fixed script

--- calculations.js
+++ calculations.js
@@ -1,23 +1,23 @@
 function increaseBalance(x, y) {
-  x = x + y;
-  return x;
-}
+  let a = x + y;
+  return a;
+} //добавила временную переменную "а", и присвоили ей значение x + y
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
-}
+function decreaseBalance(x, y) {
+  let b = x - y;
+  return b;
+} //добавила переменную y, добавила временную переменную "b" т.к. в console.log указано 2 значения 3000, 700
 
 function divideBalanceByAccounts(x, y) {
-  x = x / y;
-  return x;
+  let c = x / y;
+  return c;
   console.log("x");
-}
+} // в данном коде console.log("x") можно не писать, т.к. после return c; дальнейший код не выполняется, добавила переменную y, добавила временную переменную "b"
 
 function getRestAfterDivision(x, y) {
-  x = x % y;
-  return x;
-}
+  let d = x % y;
+  return d;
+} //добавила переменную d, добавила временную переменную "b"
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
