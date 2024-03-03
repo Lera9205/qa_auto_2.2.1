e4f80a3589217b5f85cac66bf8164153772db1a8
ValeriaIbragimova <valeria92-92+1@mail.ru>
2024-03-03 21:33:22 +0300

    2.2.4 added function with inkrement

--- calculations.js
+++ calculations.js
@@ -21,3 +21,13 @@ console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
 console.log(getRestAfterDivision(7000, 3));
+
+/*Добавить в скрипт 2 функции для нашего расчета - одну с инкрементом и вторую с декрементом, соответственно. 
+Вызовите функции и выведите в консоль их результаты.*/
+
+function increment(x) {
+  x = 5;
+  x++;
+  return x;
+}
+console.log(increment(5));
