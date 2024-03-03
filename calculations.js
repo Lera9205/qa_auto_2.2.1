38664a73df2a8ed677c2ec277c4b5e375a566266
ValeriaIbragimova <valeria92-92+1@mail.ru>
2024-03-03 20:16:57 +0300

    2.2.2 added script assignment operators

--- calculations.js
+++ calculations.js
@@ -1,24 +1,22 @@
 function increaseBalance(x, y) {
-  let a = x + y;
-  return a;
-} //добавила временную переменную "а", и присвоили ей значение x + y
+  x += y;
+  return x;
+}
 
 function decreaseBalance(x, y) {
-  let b = x - y;
-  return b;
-} //добавила переменную y, добавила временную переменную "b" т.к. в console.log указано 2 значения 3000, 700
-
+  x -= y;
+  return x;
+}
 function divideBalanceByAccounts(x, y) {
-  let c = x / y;
-  return c;
+  x /= y;
+  return x;
   console.log("x");
-} // в данном коде console.log("x") можно не писать, т.к. после return c; дальнейший код не выполняется, добавила переменную y, добавила временную переменную "b"
+}
 
 function getRestAfterDivision(x, y) {
-  let d = x % y;
-  return d;
-} //добавила переменную d, добавила временную переменную "b"
-
+  x %= y;
+  return x;
+}
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
 console.log(divideBalanceByAccounts(3000, 2, 4000));
